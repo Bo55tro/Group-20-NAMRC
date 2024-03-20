4039c9a573e03b78cb5f465f863a4dad4eb3c96a
meariba123 <122649650+meariba123@users.noreply.github.com>
2024-03-20 12:47:44 +0000

    fdn

--- NAMRC/Front end code/Department_manager/DM_Home.php
+++ NAMRC/Front end code/Department_manager/DM_Home.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en"> <!-- HIIIII --> <!-- yeeah-->
-<html lang="en"> <!-- HEY --> <!-- ariba naveed-->
+<html lang="en"> <!-- HEY --> <!-- ariba naveed...-->
 <head>
     <title>Nuclear AMRC</title>
     <meta charset="utf-8">
