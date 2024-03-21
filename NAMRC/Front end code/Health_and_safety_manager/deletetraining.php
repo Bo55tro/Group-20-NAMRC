66a38b617c100055163d154dd62449350cad125c
meariba123 <122649650+meariba123@users.noreply.github.com>
2024-03-21 19:59:29 +0000

    change

--- NAMRC/Front end code/Health_and_safety_manager/deletetraining.php
+++ NAMRC/Front end code/Health_and_safety_manager/deletetraining.php
@@ -1,5 +1,5 @@
 <?php
-include('C:\xampp\htdocs\Group-20-NAMRC\NAMRC\Front end code\Health_and_safety_manager\HS_Home.php');
+include('C:\xampp\htdocs\Group-20-NAMRC\NAMRC\Front end code\Home.html');
 
 $db = new SQLite3('C:\xampp\htdocs\Group-20-NAMRC\NAMRC\NAMRC.db');
 $sql = "SELECT training_ID, training_name FROM Training WHERE training_ID=:training_ID";
