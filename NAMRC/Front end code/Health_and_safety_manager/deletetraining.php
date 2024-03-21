77853858f35a55df2707d1a7cfb6f4bf9aeb97c3
meariba123 <122649650+meariba123@users.noreply.github.com>
2024-03-21 20:04:44 +0000

    Update deletetraining.php

--- NAMRC/Front end code/Health_and_safety_manager/deletetraining.php
+++ NAMRC/Front end code/Health_and_safety_manager/deletetraining.php
@@ -1,48 +1,54 @@
 <?php
 
 $db = new SQLite3('C:\xampp\htdocs\Group-20-NAMRC\NAMRC\NAMRC.db');
-$sql = "SELECT training_ID, training_name FROM Training WHERE training_ID=:training_ID";
-$stmt = $db->prepare($sql);
-$stmt->bindParam(':training_ID', $_GET['training_ID'], SQLITE3_INTEGER);
-$result= $stmt->execute();
-$arrayResult = [];
 
-while($row=$result->fetchArray(SQLITE3_NUM)){
-    $arrayResult [] = $row;
-}
+// Check if training_ID is provided
+if (isset($_GET['training_ID']) && is_numeric($_GET['training_ID'])) {
+    $training_ID = $_GET['training_ID'];
 
-if (isset($_POST['delete'])){
+    $sql = "SELECT training_ID, training_name FROM Training WHERE training_ID=:training_ID";
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam(':training_ID', $training_ID, SQLITE3_INTEGER);
+    $result = $stmt->execute();
+    $arrayResult = [];
 
-    $db = new SQLite3('C:\xampp\htdocs\Group-20-NAMRC\NAMRC\NAMRC.db');
+    while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+        $arrayResult[] = $row;
+    }
 
-    $stmt = "DELETE FROM Training WHERE training_ID = :training_ID";
-    $sql = $db->prepare($stmt);
-    $sql->bindParam(':training_ID', $_POST['training_ID'], SQLITE3_NUM);
-
-    $sql->execute();
+    if (isset($_POST['delete'])) {
+        $stmt = $db->prepare("DELETE FROM Training WHERE training_ID = :training_ID");
+        $stmt->bindParam(':training_ID', $training_ID, SQLITE3_INTEGER);
+        $stmt->execute();
+        header("Location: Viewtraining.php");
+        exit();
+    }
+} else {
+    echo "Invalid training ID.";
+    exit();
 }
 
 ?>
 
-<h2>Delete Training For <?php echo $_GET['training_ID'];?></h2><br>
-        <h4 style="color: red;">Are you sure want to delete this training?</h4><br>
-        
+<h2>Delete Training For <?php echo $training_ID; ?></h2><br>
+<h4 style="color: red;">Are you sure want to delete this training?</h4><br>
+
 <div class="row">
     <div class="col-11">
         <?php if (!empty($arrayResult)): ?>
             <form method="post">
-                <?php foreach($arrayResult as $row): ?>
-                <input type="hidden" name="training_name" value="<?php echo $row['training_name']; ?>">
-                <div class="form-group col-md-3">
-                    <label class="control-label labelFont">Training Name</label>
-                    <input class="form-control" type="text" name="training_name" value="<?php echo $row['training_name']; ?>">
-                </div>
+                <?php foreach ($arrayResult as $row): ?>
+                    <input type="hidden" name="training_ID" value="<?php echo $row['training_ID']; ?>">
+                    <div class="form-group col-md-3">
+                        <label class="control-label labelFont">Training Name</label>
+                        <input class="form-control" type="text" name="training_name" value="<?php echo $row['training_name']; ?>">
+                    </div>
                 <?php endforeach; ?>
-                <input type="submit" name="submit" value="Delete Training" class="btn btn-primary">
+                <input type="submit" name="delete" value="Delete Training" class="btn btn-primary">
             </form>
         <?php else: ?>
             <p>Can not delete the training you have selected.</p>
         <?php endif; ?>
     </div>
 </div>
-<div class="form-group col-md-3"><a href="Viewtraining.php">Back</a></div> 
+<div class="form-group col-md-3"><a href="Viewtraining.php">Back</a></div>
