4d426efd95522ec7464a0ab2d402156b9c395007
Scarlett Shaw <Scarlett.Shaw@student.shu.ac.uk>
2024-03-22 11:37:15 +0000

    ch

--- NAMRC/Front end code/Manufacturing_cell_manager/ViewemployeesSQL.php
+++ NAMRC/Front end code/Manufacturing_cell_manager/ViewemployeesSQL.php
@@ -8,7 +8,7 @@ function getEmployee (){
     INNER JOIN 'Operator Certification' OC ON TS.tech_ID = OC.tech_ID
     INNER JOIN Certifications Cer ON OC.certification_ID = Cer.certification_ID
     INNER JOIN Cells C ON Cer.cell_ID = C.cell_ID
-    INNER JOIN 'Manufacturing_Cell_Manager' M ON C.MCM_ID = M.MCM_ID
+    INNER JOIN 'Manufacturing Cell Manager' M ON C.MCM_ID = M.MCM_ID
     WHERE M.MCM_ID = 1;
     "; //where TS.tech_email = provided email
     $stmt = $db->prepare($sql);
