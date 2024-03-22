6f647c12b720b852943d57182ba9f1633070ffc9
Scarlett Shaw <Scarlett.Shaw@student.shu.ac.uk>
2024-03-22 12:31:54 +0000

    Update Viewcertifications.php

--- NAMRC/Front end code/Health_and_safety_manager/Viewcertifications.php
+++ NAMRC/Front end code/Health_and_safety_manager/Viewcertifications.php
@@ -1,7 +1,5 @@
 <!-- Viewing done by Ariba !-->
-<?php
-include('C:\xampp\htdocs\Group-20-NAMRC\NAMRC\Front end code\Health_and_safety_manager\HS_view.php');
-?>
+<link rel="stylesheet" href="../site.css"/>
 <div class="container pb-5">
     <main role="main" class="pb-3">
         <h2>View Certifications:</h2><br>
