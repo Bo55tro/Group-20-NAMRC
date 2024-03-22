96ff741a22ebac8d5d58f3dfecfac5625f33ba8a
Scarlett Shaw <Scarlett.Shaw@student.shu.ac.uk>
2024-03-22 10:08:22 +0000

    d

--- NAMRC/Front end code/Health_and_safety_manager/ViewcellSQL.php
+++ NAMRC/Front end code/Health_and_safety_manager/ViewcellSQL.php
@@ -3,7 +3,9 @@
 function getCell (){
     $arrayResult = array(); 
     $db = new SQLite3('C:\xampp\htdocs\Group-20-NAMRC\NAMRC\NAMRC.db');
-    $sql = "SELECT * FROM Cells";
+    $sql = "SELECT Cells.cell_ID, Cells.cell_name, Manufacturing_Cell_Manager.MCM_fname, Manufacturing_Cell_Manager.MCM_lname 
+    FROM Cells 
+    INNER JOIN Manufacturing_Cell_Manager ON Cells.MCM_ID = Manufacturing_Cell_Manager.MCM_ID";
     $stmt = $db->prepare($sql);
     $result = $stmt->execute();
 
