c5657e8303e6e08637df28172b101c885ed3817f
meariba123 <122649650+meariba123@users.noreply.github.com>
2024-03-22 11:20:52 +0000

    Update Viewtraining.php

--- NAMRC/Front end code/Health_and_safety_manager/Viewtraining.php
+++ NAMRC/Front end code/Health_and_safety_manager/Viewtraining.php
@@ -27,7 +27,7 @@
                 <a href="deletetraining.php?training_ID=<?php echo $Training[$i]['training_ID']; ?>"> Delete</a><td>
                 </body>
                 </tr>
-                <a href="C:\Users\ariba\OneDrive\Documents\GitHub\Group-20-NAMRC\NAMRC\Front end code\Health_and_safety_manager\Viewcertifications.php">Next</a>
+
 
                     <?php
                     endfor;
@@ -37,3 +37,4 @@
         </div>
     </main>
 </div>
+<a href="C:\Users\ariba\OneDrive\Documents\GitHub\Group-20-NAMRC\NAMRC\Front end code\Health_and_safety_manager\Viewcertifications.php">Next</a>
