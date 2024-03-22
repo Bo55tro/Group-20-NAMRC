7678f338437b52c0ed5ab1044912f9091331b087
meariba123 <122649650+meariba123@users.noreply.github.com>
2024-03-22 17:14:18 +0000

    delete certifications

--- NAMRC/Front end code/Health_and_safety_manager/deletecertifications.php
+++ NAMRC/Front end code/Health_and_safety_manager/deletecertifications.php
@@ -3,12 +3,12 @@
 
 $db = new SQLite3('C:\xampp\htdocs\Group-20-NAMRC\NAMRC\NAMRC.db');
 
-if (isset($_GET['training_ID']) && is_numeric($_GET['training_ID'])) {
-    $training_ID = $_GET['training_ID'];
+if (isset($_GET['certification_ID']) && is_numeric($_GET['certification_ID'])) {
+    $training_ID = $_GET['certification_ID'];
 
-    $sql = "SELECT training_ID, training_name FROM Training WHERE training_ID=:training_ID";
+    $sql = "SELECT certification_ID, certification_name, cell_ID FROM Certifications WHERE certification_ID=:certification_ID";
     $stmt = $db->prepare($sql);
-    $stmt->bindParam(':training_ID', $training_ID, SQLITE3_INTEGER);
+    $stmt->bindParam(':certification_ID', $certification_ID, SQLITE3_INTEGER);
     $result = $stmt->execute();
     $arrayResult = [];
 
@@ -17,20 +17,20 @@ if (isset($_GET['training_ID']) && is_numeric($_GET['training_ID'])) {
     }
 
     if (isset($_POST['delete'])) {
-        $stmt = $db->prepare("DELETE FROM Training WHERE training_ID = :training_ID");
-        $stmt->bindParam(':training_ID', $training_ID, SQLITE3_INTEGER);
+        $stmt = $db->prepare("DELETE FROM Certifications WHERE certification_ID = :certification_ID");
+        $stmt->bindParam(':certification_ID', $certification_ID, SQLITE3_INTEGER);
         $stmt->execute();
         header("Location: HS_Viewtraining.php");
         exit();
     }
 } else {
-    echo "Invalid training ID.";
+    echo "Invalid certification ID.";
     exit();
 }
 
 ?>
 
-<h2>Delete Training For <?php echo $training_ID; ?></h2><br>
+<h2>Delete Certification For <?php echo $certification_ID; ?></h2><br>
 <h4 style="color: red;">Are you sure want to delete this training?</h4><br>
 
 <div class="row">
@@ -38,16 +38,23 @@ if (isset($_GET['training_ID']) && is_numeric($_GET['training_ID'])) {
         <?php if (!empty($arrayResult)): ?>
             <form method="post">
                 <?php foreach ($arrayResult as $row): ?>
-                    <input type="hidden" name="training_ID" value="<?php echo $row['training_ID']; ?>">
+                    <input type="hidden" name="certification_ID" value="<?php echo $row['certification_ID']; ?>">
                     <div class="form-group col-md-3">
-                        <label class="control-label labelFont">Training Name</label>
-                        <input class="form-control" type="text" name="training_name" value="<?php echo $row['training_name']; ?>">
+                        <label class="control-label labelFont">Certification Name</label>
+                        <input class="form-control" type="text" name="certification_name" value="<?php echo $row['certification_name']; ?>">
                     </div>
+
+                    <input type="hidden" name="certification_ID" value="<?php echo $row['certification_ID']; ?>">
+                    <div class="form-group col-md-3">
+                        <label class="control-label labelFont">Cell ID</label>
+                        <input class="form-control" type="integer" name="cell_ID" value="<?php echo $row['cell_ID']; ?>">
+                    </div>
+
                 <?php endforeach; ?>
-                <input type="submit" name="delete" value="Delete Training" class="btn btn-primary">
+                <input type="submit" name="delete" value="Delete Certification" class="btn btn-primary">
             </form>
         <?php else: ?>
-            <p>Can not delete the training you have selected.</p>
+            <p>Can not delete the certification you have selected.</p>
         <?php endif; ?>
     </div>
 </div>
