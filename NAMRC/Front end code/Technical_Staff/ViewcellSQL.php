886836564714b3e15c4aabac860aa5dbe8d9c76a
Scarlett Shaw <Scarlett.Shaw@student.shu.ac.uk>
2024-03-22 11:17:56 +0000

    d

--- NAMRC/Front end code/Technical_Staff/ViewcellSQL.php
+++ NAMRC/Front end code/Technical_Staff/ViewcellSQL.php
@@ -3,7 +3,7 @@
 function getCell (){
     $arrayResult = array(); 
     $db = new SQLite3('C:\xampp\htdocs\Group-20-NAMRC\NAMRC\NAMRC.db');
-    $sql = "SELECT TS.tech_ID, TS.tech_fname, TS.tech_lname, C.cell_ID, C.cell_name
+    $sql = "SELECT C.cell_ID, C.cell_name
     FROM 'Technical Staff' TS
     INNER JOIN 'Operator Certification' OC ON TS.tech_ID = OC.tech_ID
     INNER JOIN Certifications Cert ON OC.certification_ID = Cert.certification_ID
