be4c271f6ac03ada434eb120d90850586e11b1e1
lukesingh871 <162440275+lukesingh871@users.noreply.github.com>
2024-03-23 15:02:44 +0000

    made table better to view for user

--- NAMRC/Front end code/Health_and_safety_manager/Viewtraining.php
+++ NAMRC/Front end code/Health_and_safety_manager/Viewtraining.php
@@ -36,7 +36,7 @@
             </div>
         </div>
     </main>
-    <p>Press the "Next" icon to view the list of certifications. <a href="../Health_and_safety_manager/Viewcertifications.php">Next</a></p>
+    <div class= "tablelink">Press the "Next" icon to view the list of certifications. <a href="../Health_and_safety_manager/Viewcertifications.php">Next</a></div>
 
 
 </div>
