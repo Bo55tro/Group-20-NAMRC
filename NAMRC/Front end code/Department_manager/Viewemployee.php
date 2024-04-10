eedde84a2588c1f65a36c40b192d60e2b0cee452
Scarlett Shaw <Scarlett.Shaw@student.shu.ac.uk>
2024-04-10 10:49:58 +0100

    ewdfkjbsfk

---
+++ NAMRC/Front end code/Department_manager/Viewemployee.php
@@ -0,0 +1,42 @@
+<!-- Viewing done by Ariba !-->
+
+<div class="container pb-5">
+    <main role="main" class="pb-3">
+        <h2>View Employees:</h2><br>
+
+<div class="row">
+    <div class="col-5">
+        <table class="table table-bordered table-striped">
+            <thead class="table-dark">
+                <th style="min-width: 175px;">Employee First Name</th> 
+                <th style="min-width: 175px;">Employee Last Name</th> 
+                <th style="min-width: 175px;">Employee Email</th> 
+                <td colspan="2" align="center"> Action </td>
+                </thead>
+           
+                
+           
+            <?php
+            include 'ViewemployeeSQL.php';
+            $Cell = getEmployee();
+
+                for ($i=0; $i<count($Cell); $i++):
+            ?>
+            <tr>
+                <td><?php echo $Cell[$i]['tech_fname']?></td>
+                <td><?php echo $Cell[$i]['tech_lname']?></td>
+                <td><?php echo $Cell[$i]['tech_email']?></td>
+               
+                <td colspan="2" align="center">
+                
+                </body>
+                </tr>
+
+                    <?php
+                    endfor;
+                    ?>
+                </table>
+            </div>
+        </div>
+    </main>
+</div>
