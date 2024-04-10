57bde6e0ed2305de2e6e66e67da3203445f36bdd
Scarlett Shaw <Scarlett.Shaw@student.shu.ac.uk>
2024-04-10 11:52:12 +0100

    Update Viewcell.php

--- NAMRC/Front end code/Manufacturing_cell_manager/Viewcell.php
+++ NAMRC/Front end code/Manufacturing_cell_manager/Viewcell.php
@@ -1,4 +1,6 @@
 <!-- Viewing done by Ariba !-->
+<!-- Link to Bootstrap CSS from CDN -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
 <div class="container pb-5">
     <main role="main" class="pb-3">
