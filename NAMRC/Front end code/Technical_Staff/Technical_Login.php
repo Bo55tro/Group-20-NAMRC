7a9a7a26896bdfab2eabe3aacf2ab216d1a70d3d
Scarlett Shaw <Scarlett.Shaw@student.shu.ac.uk>
2024-04-10 11:04:30 +0100

    ss

--- NAMRC/Front end code/Technical_Staff/Technical_Login.php
+++ NAMRC/Front end code/Technical_Staff/Technical_Login.php
@@ -18,7 +18,7 @@
             <div class="login_header">Login</div> 
             <div class ="Email">Email</div>
             <div class ="Log">Password</div>
-            <form method="post" action="Technical_Home.php">
+            <form method="post" action="process_login.php">
                 <div>
                 <div class="emaillabel">
                         <input type="text" id="EMAIL" name="email">
