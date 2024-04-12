b9bac2f5e1308cf5ae7febecadf0147546b07ba6
Scarlett Shaw <Scarlett.Shaw@student.shu.ac.uk>
2024-04-12 11:57:39 +0100

    Update Viewemployees.php

--- NAMRC/Front end code/Technical_Staff/Viewemployees.php
+++ NAMRC/Front end code/Technical_Staff/Viewemployees.php
@@ -1,14 +1,5 @@
 <!-- Javascript coded to retrieve technical staff trainings and certifications - ariba -->
-<?php
-session_start();
-if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
-    header("Location: login.php");
-    exit();
-}
 
-//$email = $_SESSION['email']; // Retrieve email from session
-
-?>
 
 <!DOCTYPE html>
 <html>
