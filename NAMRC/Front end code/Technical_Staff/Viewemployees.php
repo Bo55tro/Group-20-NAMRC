b12dbd438c6214eb596a10ed89f25db93156635e
Scarlett Shaw <Scarlett.Shaw@student.shu.ac.uk>
2024-04-12 11:54:52 +0100

    s

--- NAMRC/Front end code/Technical_Staff/Viewemployees.php
+++ NAMRC/Front end code/Technical_Staff/Viewemployees.php
@@ -5,7 +5,7 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
     header("Location: login.php");
     exit();
 }
-
+var_dump($_SESSION);
 $email = $_SESSION['email']; // Retrieve email from session
 
 ?>
