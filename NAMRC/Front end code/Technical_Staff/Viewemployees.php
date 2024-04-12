64cb378dfbe0b53897d94acb6cebf82c31aa2528
meariba123 <122649650+meariba123@users.noreply.github.com>
2024-04-12 14:00:51 +0100

    error fix

--- NAMRC/Front end code/Technical_Staff/Viewemployees.php
+++ NAMRC/Front end code/Technical_Staff/Viewemployees.php
@@ -10,12 +10,13 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
 $db = new SQLite3('C:\xampp\htdocs\Group-20-NAMRC\NAMRC\NAMRC.db');
 
 // Check if connection was successful
-if (!$db) {
-    echo json_encode(array("success" => false, "message" => "Connection to database failed."));
+if (!isset($_POST["email"])) {
+    echo json_encode(array("success" => false, "message" => "Email address is required."));
     exit();
 }
 
 $email = $_POST["email"];
+
 
 // SQL query to select training and certifications based on email
 $stmt = $db->prepare("SELECT `Technical Staff`.`tech_fname`, `Technical Staff`.`tech_lname`, `Training`.`training_name`
@@ -23,11 +24,18 @@ FROM `Technical Staff`
 INNER JOIN `Operator Training` ON (`Technical Staff`.`tech_ID` = `Operator Training`.`tech_ID`)
 INNER JOIN `Training` ON (`Operator Training`.`training_ID` = `Training`.`training_ID`)
 WHERE `Technical Staff`.`tech_email` = :email;");
+$stmt->bindValue(':email', $email, SQLITE3_TEXT);
+$result = $stmt->execute();
 
 
 $data = array();
-while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
-    $data[] = $row;
+if ($result) {
+    while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+        $data[] = $row;
+    }
+} else {
+    echo json_encode(array("success" => false, "message" => "Failed to execute SQL query."));
+    exit();
 }
 
 $db->close();
