08156b71baa255e8ce8dc018faf9cf27a384a527
meariba123 <122649650+meariba123@users.noreply.github.com>
2024-04-12 13:58:14 +0100

    changing select statements

--- NAMRC/Front end code/Technical_Staff/Viewemployees.php
+++ NAMRC/Front end code/Technical_Staff/Viewemployees.php
@@ -18,13 +18,12 @@ if (!$db) {
 $email = $_POST["email"];
 
 // SQL query to select training and certifications based on email
-$stmt = $db->prepare("SELECT Technical Staff.tech_fname, Technical Staff.tech_lname, Training.training_name
-FROM Technical Staff
-INNER JOIN Operator Training ON (Technical Staff.tech_ID = Operator Training.tech_ID)
-INNER JOIN Training ON (Operator Training.training_ID = Training.training_ID)
-WHERE Technical Staff.tech_email = :email;");
-$stmt->bindValue(':email', $email, SQLITE3_TEXT);
-$result = $stmt->execute();
+$stmt = $db->prepare("SELECT `Technical Staff`.`tech_fname`, `Technical Staff`.`tech_lname`, `Training`.`training_name`
+FROM `Technical Staff`
+INNER JOIN `Operator Training` ON (`Technical Staff`.`tech_ID` = `Operator Training`.`tech_ID`)
+INNER JOIN `Training` ON (`Operator Training`.`training_ID` = `Training`.`training_ID`)
+WHERE `Technical Staff`.`tech_email` = :email;");
+
 
 $data = array();
 while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
