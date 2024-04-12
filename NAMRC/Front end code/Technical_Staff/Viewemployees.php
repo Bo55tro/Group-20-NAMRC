89ef1e97dcffec8ff61bd509d59a46954b913963
Scarlett Shaw <Scarlett.Shaw@student.shu.ac.uk>
2024-04-12 12:11:52 +0100

    Update Viewemployees.php

--- NAMRC/Front end code/Technical_Staff/Viewemployees.php
+++ NAMRC/Front end code/Technical_Staff/Viewemployees.php
@@ -2,7 +2,7 @@
 <?php
 
 if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
-    header("Location: Technical_Login");
+    header("Location: Technical_Login.php");
     exit();
 }
 
