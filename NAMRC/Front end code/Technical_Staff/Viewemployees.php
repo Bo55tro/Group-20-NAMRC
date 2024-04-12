310f9449506243ec337bca7bbee0181e4a7b5a9c
meariba123 <122649650+meariba123@users.noreply.github.com>
2024-04-12 20:38:49 +0100

    Update Viewemployees.php

--- NAMRC/Front end code/Technical_Staff/Viewemployees.php
+++ NAMRC/Front end code/Technical_Staff/Viewemployees.php
@@ -1,6 +1,4 @@
 <?php
-session_start();
-
 // Check if user is logged in and email is set in the session
 if (!isset($_SESSION['logged_in']) || !isset($_SESSION['email'])) {
     // Redirect the user to the login page or display an error message
@@ -11,6 +9,14 @@ if (!isset($_SESSION['logged_in']) || !isset($_SESSION['email'])) {
 // Retrieve the email address from the session
 $email = $_SESSION['email'];
 
+// Create a new SQLite3 database connection
+$db = new SQLite3('C:\xampp\htdocs\Group-20-NAMRC\NAMRC\NAMRC.db');
+
+// Check if connection was successful
+if (!$db) {
+    die("Connection failed: " . $db->lastErrorMsg());
+}
+
 // SQL query to select training and certifications based on email
 $stmt = $db->prepare("SELECT `Technical Staff`.`tech_fname`, `Technical Staff`.`tech_lname`, `Training`.`training_name`
 FROM `Technical Staff`
@@ -19,7 +25,6 @@ INNER JOIN `Training` ON (`Operator Training`.`training_ID` = `Training`.`traini
 WHERE `Technical Staff`.`tech_email` = :email;");
 $stmt->bindValue(':email', $email, SQLITE3_TEXT);
 $result = $stmt->execute();
-
 
 $data = array();
 if ($result) {
