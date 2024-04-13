ddc02b98b9bb987fecb247a694fe9c0d702bc8c5
meariba123 <122649650+meariba123@users.noreply.github.com>
2024-04-13 14:28:16 +0100

    Update Viewemployees.php

--- NAMRC/Front end code/Technical_Staff/Viewemployees.php
+++ NAMRC/Front end code/Technical_Staff/Viewemployees.php
@@ -22,8 +22,10 @@ $stmt = $db->prepare("SELECT `Technical Staff`.`tech_fname`, `Technical Staff`.`
 FROM `Technical Staff`
 INNER JOIN `Operator Training` ON (`Technical Staff`.`tech_ID` = `Operator Training`.`tech_ID`)
 INNER JOIN `Training` ON (`Operator Training`.`training_ID` = `Training`.`training_ID`)
-INNER JOIN `Certifications` ON (`Technical Staff`.`tech_ID` = `Certification`.`tech_ID`)
+INNER JOIN `Operator Certification` ON (`Technical Staff`.`tech_ID` = `Operator Certification`.`tech_ID`)
+INNER JOIN `Certifications` ON (`Operator Certification`.`certification_ID` = `Certification`.`certification_ID`)
 WHERE `Technical Staff`.`tech_email` = :email;");
+
 $stmt->bindValue(':email', $email, SQLITE3_TEXT);
 $result = $stmt->execute();
 
