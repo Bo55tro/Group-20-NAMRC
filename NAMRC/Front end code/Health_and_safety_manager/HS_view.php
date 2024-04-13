b67de74023291c42706289e5294b1d70f1e4e9af
meariba123 <122649650+meariba123@users.noreply.github.com>
2024-04-13 19:42:54 +0100

    view employees

--- NAMRC/Front end code/Health_and_safety_manager/HS_view.php
+++ NAMRC/Front end code/Health_and_safety_manager/HS_view.php
@@ -1,5 +1,5 @@
 <?php
-include('C:\xampp\htdocs\Group-20-NAMRC\NAMRC\Front end code\Health_and_safety_manager\Viewtraining.php');
+include('C:\xampp\htdocs\Group-20-NAMRC\NAMRC\Front end code\Health_and_safety_manager\Viewemployees.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
