ded3b21af7c255cfa7328b7c8d4e184a42630648
meariba123 <122649650+meariba123@users.noreply.github.com>
2024-04-13 19:48:45 +0100

    Update ViewemployeesSQL.php

--- NAMRC/Front end code/Health_and_safety_manager/ViewemployeesSQL.php
+++ NAMRC/Front end code/Health_and_safety_manager/ViewemployeesSQL.php
@@ -3,17 +3,13 @@
 function getEmployee (){
     $arrayResult = array(); 
     $db = new SQLite3('C:\xampp\htdocs\Group-20-NAMRC\NAMRC\NAMRC.db');
-    $sql = "SELECT T.tech_fname, T.tech_lname, T.tech_email
-    FROM 'Technical Staff' T
-    JOIN 'Departments' D ON T.DEP_ID = D.DEP_ID
-    JOIN 'Department Managers' M ON D.DM_ID = M.DM_ID
-    WHERE M.DM_ID = 1;
-     ";
+    $sql = "SELECT tech_fname, tech_lname, tech_email
+            FROM 'Technical Staff'";
     $stmt = $db->prepare($sql);
     $result = $stmt->execute();
 
-    while ($row = $result->fetchArray()){ 
-        $arrayResult [] = $row; 
+    while ($row = $result->fetchArray()) { 
+        $arrayResult[] = $row; 
     }
     return $arrayResult;
 }
