f40d9d257d75973992dab5423a648203390461c5
lukesingh871 <162440275+lukesingh871@users.noreply.github.com>
2024-04-14 23:04:36 +0100

    made it so that HS can view all the staff rather than just technical

--- NAMRC/Front end code/Health_and_safety_manager/ViewemployeesSQL.php
+++ NAMRC/Front end code/Health_and_safety_manager/ViewemployeesSQL.php
@@ -1,16 +1,59 @@
 <!-- Viewing done by Ariba !-->
+
 <?php
 function getEmployee (){
     $arrayResult = array(); 
     $db = new SQLite3('C:\xampp\htdocs\Group-20-NAMRC\NAMRC\NAMRC.db');
-    $sql = "SELECT tech_fname, tech_lname, tech_email
-            FROM 'Technical Staff'";
+    $sql = "SELECT * FROM 'Technical Staff';";
     $stmt = $db->prepare($sql);
     $result = $stmt->execute();
 
-    while ($row = $result->fetchArray()) { 
-        $arrayResult[] = $row; 
+    while ($row = $result->fetchArray()){ 
+        $arrayResult [] = $row; 
+    }
+    return $arrayResult;
+}
+
+function getMCM (){
+    $arrayResult = array(); 
+    $db = new SQLite3('C:\xampp\htdocs\Group-20-NAMRC\NAMRC\NAMRC.db');
+    $sql = "SELECT * FROM 'Manufacturing Cell Manager';";
+    $stmt = $db->prepare($sql);
+    $result = $stmt->execute();
+
+    while ($row = $result->fetchArray()){ 
+        $arrayResult [] = $row; 
+    }
+    return $arrayResult;
+}
+
+function getDM (){
+    $arrayResult = array(); 
+    $db = new SQLite3('C:\xampp\htdocs\Group-20-NAMRC\NAMRC\NAMRC.db');
+    $sql = "SELECT * FROM 'Department Managers';";
+    $stmt = $db->prepare($sql);
+    $result = $stmt->execute();
+
+    while ($row = $result->fetchArray()){ 
+        $arrayResult [] = $row; 
+    }
+    return $arrayResult;
+}
+
+function getHSM (){
+    $arrayResult = array(); 
+    $db = new SQLite3('C:\xampp\htdocs\Group-20-NAMRC\NAMRC\NAMRC.db');
+    $sql = "SELECT * FROM 'Health and Safety Managers, Officers';";
+    $stmt = $db->prepare($sql);
+    $result = $stmt->execute();
+
+    while ($row = $result->fetchArray()){ 
+        $arrayResult [] = $row; 
     }
     return $arrayResult;
 }
 ?>
+
+
+
+
