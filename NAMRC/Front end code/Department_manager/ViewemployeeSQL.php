68f03b4c24d39795c137eaae1eb17d53a3361247
lukesingh871 <162440275+lukesingh871@users.noreply.github.com>
2024-04-15 10:36:40 +0100

    made DM view employee show more than just technical

--- NAMRC/Front end code/Department_manager/ViewemployeeSQL.php
+++ NAMRC/Front end code/Department_manager/ViewemployeeSQL.php
@@ -1,14 +1,36 @@
 <!-- Viewing done by Ariba !-->
+
 <?php
 function getEmployee (){
     $arrayResult = array(); 
     $db = new SQLite3('C:\xampp\htdocs\Group-20-NAMRC\NAMRC\NAMRC.db');
-    $sql = "SELECT T.tech_fname, T.tech_lname, T.tech_email
-    FROM 'Technical Staff' T
-    JOIN 'Departments' D ON T.DEP_ID = D.DEP_ID
-    JOIN 'Department Managers' M ON D.DM_ID = M.DM_ID
-    WHERE M.DM_ID = 1;
-     ";
+    $sql = "SELECT * FROM 'Technical Staff';";
+    $stmt = $db->prepare($sql);
+    $result = $stmt->execute();
+
+    while ($row = $result->fetchArray()){ 
+        $arrayResult [] = $row; 
+    }
+    return $arrayResult;
+}
+
+function getMCM (){
+    $arrayResult = array(); 
+    $db = new SQLite3('C:\xampp\htdocs\Group-20-NAMRC\NAMRC\NAMRC.db');
+    $sql = "SELECT * FROM 'Manufacturing Cell Manager';";
+    $stmt = $db->prepare($sql);
+    $result = $stmt->execute();
+
+    while ($row = $result->fetchArray()){ 
+        $arrayResult [] = $row; 
+    }
+    return $arrayResult;
+}
+
+function getDM (){
+    $arrayResult = array(); 
+    $db = new SQLite3('C:\xampp\htdocs\Group-20-NAMRC\NAMRC\NAMRC.db');
+    $sql = "SELECT * FROM 'Department Managers';";
     $stmt = $db->prepare($sql);
     $result = $stmt->execute();
 
@@ -18,3 +40,7 @@ function getEmployee (){
     return $arrayResult;
 }
 ?>
+
+
+
+
