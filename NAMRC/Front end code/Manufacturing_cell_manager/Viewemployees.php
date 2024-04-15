fde8f36341d5700493864f36fef395ec50dddbe6
lukesingh871 <162440275+lukesingh871@users.noreply.github.com>
2024-04-15 09:52:40 +0100

    updating MCM view so it can see more than just technical

--- NAMRC/Front end code/Manufacturing_cell_manager/Viewemployees.php
+++ NAMRC/Front end code/Manufacturing_cell_manager/Viewemployees.php
@@ -10,6 +10,7 @@
     <div class="col-5">
         <table class="table table-bordered table-striped">
             <thead class="table-dark">
+                <br>
                 <th style="min-width: 75px;">Tech ID</th> 
                 <th style="min-width: 75px;">First Name</th>
                 <th style="min-width: 75px;">Middle Name</th>  
@@ -55,6 +56,53 @@
                 endfor;
                 ?>
                     
+                </table>
+                
+            </div>
+        </div>
+
+        <h2>View MCM:</h2><br>
+
+        <div class="row">
+            <div class="col-5">
+                <table class="table table-bordered table-striped">
+                    <thead class="table-dark">
+                        <tr>
+                            <th style="min-width: 75px;">MCM ID</th> 
+                            <th style="min-width: 75px;">First Name</th>
+                            <th style="min-width: 75px;">Middle Name</th>  
+                            <th style="min-width: 75px;">Last Name</th> 
+                            <th style="min-width: 175px;">Email</th> 
+                            <th style="min-width: 75px;">Password</th>
+                            <th style="min-width: 75px;">Address ID</th>  
+                            <th style="min-width: 75px;">Dob</th> 
+                            <th style="min-width: 75px;">Update</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        
+                        $MCM = getMCM();
+
+                        for ($i=0; $i<count($MCM); $i++):
+                        ?>
+                        <tr>
+                            <td><?php echo $MCM[$i]['MCM_ID']?></td>
+                            <td><?php echo $MCM[$i]['MCM_fname']?></td>
+                            <td><?php echo $MCM[$i]['MCM_mname']?></td>
+                            <td><?php echo $MCM[$i]['MCM_lname']?></td>
+                            <td><?php echo $MCM[$i]['MCM_email']?></td>
+                            <td><?php echo $MCM[$i]['MCM_password']?></td>
+                            <td><?php echo $MCM[$i]['address_id']?></td>
+                            <td><?php echo $MCM[$i]['MCM_dob']?></td>
+                            <td><a href="MCM_Update.php?MCM_ID=<?php echo $MCM[$i]['MCM_ID'];?>">Update</a></td>
+
+
+                        </tr>
+                        <?php
+                        endfor;
+                        ?>
+                    </tbody>
                 </table>
             </div>
         </div>
--- NAMRC/Front end code/Manufacturing_cell_manager/ViewemployeesSQL.php
+++ NAMRC/Front end code/Manufacturing_cell_manager/ViewemployeesSQL.php
@@ -20,4 +20,17 @@ function getEmployee (){
     }
     return $arrayResult;
 }
+
+function getMCM (){
+    $arrayResult = array(); 
+    $db = new SQLite3('C:\xampp\htdocs\Group-20-NAMRC\NAMRC\NAMRC.db');
+    $sql = "SELECT * FROM 'Manufacturing Cell Manager';";
+    $stmt = $db->prepare($sql);
+    $result = $stmt->execute();
+
+    while ($row = $result->fetchArray()){ 
+        $arrayResult [] = $row; 
+    }
+    return $arrayResult;
+}
 ?>
